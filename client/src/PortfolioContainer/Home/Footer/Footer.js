3dff8ee5f29232a1c267584594a4b5efb9e4a634
Anshumali Shaw <shaw.anshu@gmail.com>
2025-12-01 00:14:14 +0530

    Added updated changes

--- client/src/PortfolioContainer/Home/Footer/Footer.js
+++ client/src/PortfolioContainer/Home/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Footer.css';
+import shapebg from "../../../assets/Home/shapebg.png";
 
 export default function Footer() {
   return (
     <div className='footer-container'>
         <div className='footer-parent'>
-            <img src={require("../../../assets/Home/shapebg.png")} alt="Not loaded" />
+            <img src={shapebg} alt="Not loaded" />
 
         </div>
 
